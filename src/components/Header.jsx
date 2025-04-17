6ff1be8e056b9cad33af9e12954396b0482101c8
sconte <contesl@gmail.com>
2025-04-17 09:19:51 -0300

    imagen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import banner from "/src/img/PersonalStyle-Banner.jpg";
 
 const Header = () => {
   return (
     <header>
       <figure className="contenedorImagen">
-        <img id="imagen-index" src="/src/img/PersonalStyle-Banner.jpg" alt="Banner" />
+        <img id="imagen-index" src={banner} alt="Banner" />
       </figure>
     </header>
   );
