e350c2d35dcc805fe1c48b54dcb5cf874d9e3125
sconte <contesl@gmail.com>
2025-04-17 08:28:16 -0300

    imagenes en inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+//cambio imagen
 const Inicio = () => {
   return (
     <main>
       <figure className="contenedorImagen centered">
-        <img id="imagen-index" src="/src/img/PersonalStyle.jpg" alt="Personal Style" />
+        <img id="imagen-index" src="../img/PersonalStyle.jpg" alt="Personal Style" />
       </figure>
     </main>
   );
